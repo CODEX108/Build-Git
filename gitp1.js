4641c3bf4c50f850d02e84d5d9f4d18e1729f587
Manjiri Milind Chavande <manjiri.c@fyle.in>
2023-07-11 15:25:02 +0530

    refactored the code to ES6 v

--- gitp1.js
+++ gitp1.js
@@ -1,39 +1,131 @@
-function Git(repoName){
-this.repoName = repoName;
-this.lastCommitId = -1;
+/**
+ * Commit class
+ * A single commit.
+ *
+ * @param {number} id  		 ID of commit.
+ * @param {Commit} parent	 Parent Commit.
+ * @param {string} msg 		 Commit message.
+ */
+class Commit {
+    constructor(id, parent, message) {
+        this.id = id;
+        this.parent = parent;
+        this.message = message;
+    }
+}
+
+class Branch {
+    constructor(name, commit) {
+        this.name = name,
+            this.commit = commit
+    }
 };
 
-//actual command > git init
-const repo = new Git('myRepo');
 
-//add the ability on our Git class to create a commit or add a commit
-Git.prototype.commit = function(message){
-   let commit = new Commit(++this.lastCommitId,message);
-    return commit;
-};
 
-Git.prototype.log = function(){
-    // Start from HEAD commit
-    history = [];
-   // 1. Start from last commit
-  // 2. Go back tracing to the first commit
-  // 3. Push in `history`
-    return history;
-};
+/**
+ * Git class
+ * Represents a Git repository.
+ *
+ * @param {string} name Repository name.
+ */
+class Git {
+    constructor(name) {
+        this.name = name; // Repo name
+        this.lastCommitId = -1; // Keep track of last commit id.
+        this.branches = []; //list of all branches
 
-function Commit(id,message){
-this.id = id;
-this.message = message;
-};
+        let master = new Branch('master', null); // null is passed as we don't have any commit yet.
+        this.branches.push(master); //store master branch
+        this.HEAD = master; // Reference to the current branch.
+    }
+
+    /**
+* Make a commit.
+* @param  {string} message Commit message.
+* @return {Commit}         Created commit object.
+*/
+    commit(message) {
+        // Increment last commit id and pass into new commit.
+        var commit = new Commit(++this.lastCommitId, this.HEAD.commit, message);
+
+        // Update HEAD and current branch.
+        this.HEAD.commit = commit;
+
+        return commit;
+    };
+
+    checkout(branchName) {
+        // Check if a branch already exists with name = branchName
+        for (let i = this.branches.length; i--;) {
+            if (this.branches[i].name === branchName) {
+                //if branch exists, update HEAD to point to this branch
+                // We found an existing branch
+                console.log("Switched to existing branch: " + branchName);
+                this.HEAD = this.branches[i];
+                return this;
+            }
+        }
+
+        // We reach here when no matching branch is found
+        //create a new branch
+        let newBranch = new Branch(branchName, this.HEAD.commit);
+        // Store branch.
+        this.branches.push(newBranch);
+        // Update HEAD
+        this.HEAD = newBranch;
+        console.log("Created new branch: " + branchName);
+        return this;
+    }
+
+    /**
+ * Logs history.
+ * @return {Array} Commits in reverse chronological order.
+ */
+    gitLog() {
+        // Start from HEAD
+        let commit = this.HEAD.commit,
+            history = [];
+
+        while (commit) {
+            history.push(commit);
+            // Keep following the parent
+            commit = commit.parent;
+        }
+
+        return history;
+    };
+
+}
 
 // Expose Git class on window.
 window.Git = Git;
+console.log("3. Branches test");
 
-console.log("Git.log() test");
+let repo = new Git("test");
 repo.commit("Initial commit");
 repo.commit("Change 1");
 
-var log = repo.log();
-console.assert(log.length === 2); // Should have 2 commits.
-console.assert(!!log[0] && log[0].id === 1); // Commit 1 should be first.
-console.assert(!!log[1] && log[1].id === 0); // And then Commit 0.
+// Maps the array of commits into a string of commit ids.
+// For [C2, C1,C3], it returns "2-1-0"
+function historyToIdMapper(history) {
+    var ids = history.map(function (commit) {
+        return commit.id;
+    });
+    return ids.join("-");
+}
+
+console.assert(historyToIdMapper(repo.gitLog()) === "1-0"); // Should show 2 commits.
+
+repo.checkout("testing");
+repo.commit("Change 3");
+
+
+console.assert(historyToIdMapper(repo.gitLog()) === "2-1-0"); // Should show 3 commits.
+
+repo.checkout("master");
+console.assert(historyToIdMapper(repo.gitLog()) === "1-0"); // Should show 2 commits. Master unpolluted.
+
+repo.commit("Change 3");
+console.assert(historyToIdMapper(repo.gitLog()) === "3-1-0"); // Continue on master with 4th commit.
+
